297493219c91886e1f8d752fac906f9bbb139f21
AhnKwangHyun <agh0314@gmail.com>
2024-02-04 04:36:36 +0900

    first commit

---
+++ src/test/java/com/revis/faddy/FaddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revis.faddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
