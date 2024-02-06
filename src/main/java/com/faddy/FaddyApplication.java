ae6ca756159a44b9d2388e46d29fad73dffb8db4
AhnKwangHyun <agh0314@gmail.com>
2024-02-06 10:35:44 +0900

    초기개발환경 설정 완료

---
+++ src/main/java/com/faddy/FaddyApplication.java
@@ -0,0 +1,13 @@
+package com.faddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaddyApplication.class, args);
+	}
+
+}
